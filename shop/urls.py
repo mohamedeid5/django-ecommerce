7be0e6970fae58a9c96b4b6b33caa1e39d105311
mohamed eid <meid368@gmail.com>
2021-07-18 07:25:25 +0200

    cart

--- shop/urls.py
+++ shop/urls.py
@@ -22,7 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('cart/', include('cart.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
